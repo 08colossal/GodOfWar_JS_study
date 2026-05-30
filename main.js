e481bae4408217dceaff0fb75b22a55a33955b2b
Giovana Pauvels <pauvelsegiovana@gmail.com>
2026-05-30 01:54:27 -0300

    Versão 1.0

---
+++ main.js
@@ -0,0 +1,46 @@
+﻿/*------------------------------------------------------------------------------*/
+/*              Procurar personagem na 'tabela' personas!                       */
+
+import persona from "./socorro.js";
+
+const pesquisa = document.getElementById('pesquisa');
+const btn = document.getElementById('btn');
+const character = document.getElementById('character');
+
+function exibir(persona){
+    if (!persona){
+        character.innerHTML = `<p>Personagem não encontrado.</p>`;
+        return;
+    }
+    
+    character.innerHTML = 
+    `
+    <strong><h2>${persona.nome}</h2></strong>
+    <img src="${persona.foto}">
+    <h3>${persona.cidadania}</h3>
+    <i><blockquote>${persona.frase}</blockquote></i>
+    
+    
+    `;
+    
+}
+
+function busca(){
+
+    const entrada = pesquisa.value.toLowerCase();
+    const saida = persona.find(i => i.nome.toLowerCase() === entrada);
+
+    exibir(saida);
+}
+
+
+//botão funcionar com Enter
+btn.addEventListener('click', busca);
+pesquisa.addEventListener('keypress', function(event){
+    if (event.key === 'Enter' && pesquisa.value != null){
+        btn.click();
+    }
+});
+
+/*------------------------------------------------------------------------------*/
+/*                                                                             */
---
+++ socorro.js
@@ -0,0 +1,71 @@
+﻿const persona = [
+    {
+        id:"kratos",
+        nome:"Kratos",
+        cidadania: 
+          ` <img src="https://static.wikia.nocookie.net/godofwar/images/7/70/Lambdasymbol1.png" width="20"> Spartan (formerly)
+            <img src="https://static.wikia.nocookie.net/godofwar/images/d/db/GOW_Omega_collection_symbol2.png" width="20"> Olympian (formerly)
+            <img src="https://static.wikia.nocookie.net/godofwar/images/8/80/RuneMidgard.webp" width="20"> Midgardian
+          `,
+        frase:"What can I say to you? I remember how it felt to take the throne. All that it meant. And all that it did not. A God of war... a God of pain. Of suffering. Of destruction. The Norns said I chase a redemption that I know I can never deserve. What does that make me? God of fools. A God of... hope. 'When all else is lost...' You lost everything. And everyone. And you became... There is no forgiving you. You chose... I chose. What now? Should I, this same man, should I sit? Take? Proclaim? Lead? Place myself in service? In service. Should I... lose everything and everyone... will there still be enough left inside so that I do not become you? I do not know. But I have hope. You are cruel, and arrogant, and selfish. But... you are more than that. You have always been more than what others saw. You are more than that.",
+        foto: "https://static.wikia.nocookie.net/godofwar/images/e/e9/Kratos-_GOW_Ragnarok.png/revision/latest?cb=20230115190836"
+   },
+   {
+        id: "aphrodite",
+        nome: "Aphrodite",
+        cidadania: ` <img src="https://static.wikia.nocookie.net/godofwar/images/d/db/GOW_Omega_collection_symbol2.png/revision/latest/scale-to-width-down/25?cb=20240103191556" width="20"> Olympian`,
+        frase: "Kratos, it's been far too long. Even though you are no longer the God of War, you can still share my bed.",
+        foto: "https://static.wikia.nocookie.net/godofwar/images/8/80/Aphrodite1.png/revision/latest?cb=20231211113104"
+   },
+   {
+        id: "zeus",
+        nome: "Zeus",
+        cidadania: ` <img src="https://static.wikia.nocookie.net/godofwar/images/d/db/GOW_Omega_collection_symbol2.png/revision/latest/scale-to-width-down/25?cb=20240103191556" width="20"> Olympian`,
+        frase: "	My brothers. We were forged in victory. A victory that ended the great war and brought forth the reign of mount Olympus. Born from the depths of the underworld, rooted in the river of souls, our mountain emerged out of the chaos. As it grew, so too did the might of the Olympians. We created a world of peace, a world of prosperity. A world that lives in the shadow and safety of my mountain. A mountain that has come to be the absolute measure of strength and power. Now, on this day, that power is to be tested. The mortal, Kratos, seeks to destroy all that I have wrought. Brothers, put aside the petty grievances that have splintered us for so long. We will unite. We will stand together. And I will wipe out this plague! Olympus will prevail!",
+        foto: "https://static.wikia.nocookie.net/godofwar/images/8/8b/AkinZeus.png/revision/latest?cb=20240128185031"
+   },
+   {
+        id: "athena",
+        nome: "Athena",
+        cidadania: ` <img src="https://static.wikia.nocookie.net/godofwar/images/d/db/GOW_Omega_collection_symbol2.png/revision/latest/scale-to-width-down/25?cb=20240103191556" width="20"> Olympian`,
+        frase: "There's nowhere you can hide, Spartan. Put as much distance between you and the truth as you want, it changes nothing. Pretend to be everything you are not: teacher, husband, father. But there is one unavoidable truth you will never escape: You cannot change. You will always be... a monster.",
+        foto: "https://static.wikia.nocookie.net/godofwar/images/6/6f/Athena_%282%29.jpg/revision/latest/scale-to-width-down/1000?cb=20191002071410"
+   },
+   {
+        id: "ares",
+        nome: "Ares",
+        cidadania: ` <img src="https://static.wikia.nocookie.net/godofwar/images/d/db/GOW_Omega_collection_symbol2.png/revision/latest/scale-to-width-down/25?cb=20240103191556" width="20"> Olympian`,
+        frase: "I have taught you many ways to kill a mortal, Kratos. Flesh that burns, bones that break. But to break a man's spirit, is to truly destroy him.",
+        foto: "https://static.wikia.nocookie.net/godofwar/images/4/44/AkinAres.png/revision/latest?cb=20240129205106"
+   },
+   {
+        id: "cronos",
+        nome: "Cronos",
+        cidadania: "",
+        frase: "You killed Ares out of your need for vengeance. But this time, retribution finally comes to me, Spartan.",
+        foto: "https://static.wikia.nocookie.net/godofwar/images/f/fd/Cronoss.jpg/revision/latest?cb=20110424201653"
+   },
+   {
+        id: "medusa",
+        nome: "Medusa",
+        cidadania: "",
+        frase: "Medusa, the Queen of the Gorgons. Bring me her head Kratos, and I will give you the ability to wield its power.",
+        foto: "https://static.wikia.nocookie.net/godofwar/images/e/e2/Medusa_%283%29.webp/revision/latest?cb=20240222150944"
+   },
+   {
+        id: "hades",
+        nome: "Hades",
+        cidadania: `<img src="https://static.wikia.nocookie.net/godofwar/images/d/db/GOW_Omega_collection_symbol2.png/revision/latest/scale-to-width-down/25?cb=20240103191556" width="20"> Olympian`,
+        frase: "Kratos, so glad you could carve out some time for us! You know we need it. I sense some bad blood between us, Kratos. Oh, all the memories. They're overwhelming, really. Let's see... How many sins have you committed against me? Oh, that's right, you murdered my niece, Athena! And what else. WHAT else? Ah, and you killed my brother, Poseidon. And I have not forgotten that it was you who butchered my beautiful Queen! I will see you suffer as I have suffered. Your soul is MINE.",
+        foto: "https://static.wikia.nocookie.net/godofwar/images/2/26/Hades_-_Ascension.jpg/revision/latest?cb=20130310194617"
+   },
+   {
+        id: "gaia",
+        nome: "Gaia",
+        cidadania: "",
+        frase: "I am the Titan, Gaia; ever-present as the Mother of Earth. I have watched you become a powerful warrior, and I have been with you through all the events of your life, but I can no longer simply watch.",
+        foto: "https://static.wikia.nocookie.net/godofwar/images/b/bb/Gow2-gaia.jpg/revision/latest?cb=20080308184943"
+   }
+];
+// Exporta os dados para que o script principal possa usar
+export default persona;
